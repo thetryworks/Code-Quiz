b1a2ade38fb3822f7c72799f202cb8b97eee8ce1
thetryworks <natesmyth1@gmail.com>
2022-03-11 13:02:11 -0600

    added seperate js file with questions

---
+++ assets/js/question.js
@@ -0,0 +1,42 @@
+var questions = [
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<script>", "<javascript>", "<css>", "<github>"],
+        answer: "<script>"
+    }
+]
+var questions = [
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<script>", "<javascript>", "<css>", "<github>"],
+        answer: "<script>"
+    }
+]
+var questions = [
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<script>", "<javascript>", "<css>", "<github>"],
+        answer: "<script>"
+    }
+]
+var questions = [
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<script>", "<javascript>", "<css>", "<github>"],
+        answer: "<script>"
+    }
+]
+var questions = [
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<script>", "<javascript>", "<css>", "<github>"],
+        answer: "<script>"
+    }
+]
+var questions = [
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<script>", "<javascript>", "<css>", "<github>"],
+        answer: "<script>"
+    }
+];

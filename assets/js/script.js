811e7a477cb558ad3839af875a3c721817d53bfb
thetryworks <natesmyth1@gmail.com>
2022-03-13 15:02:08 -0500

    finished writing the questions

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var helloEl = document.queryselector("#hello");
+var startQuizButtonEl = document.querySelector("#startQuiz");
+
+var quizEl = document.queryselector("quiz");
+
+console.log(welcomeEl);

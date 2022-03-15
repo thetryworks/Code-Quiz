2912ad8a2ce7fe5bfc972ca92d31459558108336
thetryworks <natesmyth1@gmail.com>
2022-03-15 14:59:42 -0500

    finished javascript

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,37 +3,37 @@ var questions = [
         title: "Inside which HTML element do we put the JavaScript?",
         choices: ["<script>", "<javascript>", "<css>", "<github>"],
         answer: "<script>"
-    }
-]
-var questions = [
+    },
+
+
     {
         title: "How do you write \"Hello World\" in an alert box? ",
         choices: ["msgBox(\"Hello World\")", "alertBox(\"Hello World\")", "msg(\"Hello World\")", "alert(\"Hello World\")"],
         answer: "alert(\"Hello World\")"
-    }
-]
-var questions = [
+    },
+
+
     {
         title: "Choose the correct HTML element for the largest heading",
         choices: ["<head>", "<h3>", "<heading>", "<h1>"],
         answer: "<h1>"
-    }
-]
-var questions = [
+    },
+
+
     {
         title: "How do you create an unordered list?",
         choices: ["<ol>", "<list>", "<unlist>", "<ul>"],
         answer: "<ul>"
-    }
-]
-var questions = [
+    },
+
+
     {
         title: "What does CSS stand for?",
         choices: ["Cascading Style Sheets", "Call Sign Segment", "Callback Symbol Statement", "Create Style Symbols"],
         answer: "Cascading Style Sheets"
-    }
-]
-var questions = [
+    },
+
+
     {
         title: "Which of these is not a programming language",
         choices: ["Java", "Ruby", "Python", "Cottonmouth"],
